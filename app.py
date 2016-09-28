0eac7a238ac9b6cabe6bda2701db6ffa9a1e429a
virajnilakh <virajnilakh220@gmail.com>
2016-09-28 15:45:01 -0700

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello():
     return "Hello from Dockerized Flask App!!"
 
+@app.route("/v1")
+def v1():
+    return "Hello v1!"
+
+
+
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
